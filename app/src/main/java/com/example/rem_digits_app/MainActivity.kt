04715793175f29341ff8b51714e131c4958ce0cc
luis <killuari@protonmail.com>
2022-04-10 15:45:25 +0200

    added activities and navigation between them

--- app/src/main/java/com/example/rem_digits_app/MainActivity.kt
+++ app/src/main/java/com/example/rem_digits_app/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.rem_digits_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rem_digits_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.training.setOnClickListener{
+            startActivity(Intent(this, TrainingActivity::class.java))
+        }
+
+        binding.challenge.setOnClickListener{
+            startActivity(Intent(this, ChallengeActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/rem_digits_app/TrainingActivity.kt
@@ -0,0 +1,31 @@
+package com.example.rem_digits_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.rem_digits_app.databinding.ActivityTrainingBinding
+
+class TrainingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTrainingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTrainingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.topAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.pi -> {
+                    // Handle edit text press
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
