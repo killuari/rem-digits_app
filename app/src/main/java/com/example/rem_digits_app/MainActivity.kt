26311d996c2a421e4b3b5785c634291c41c4f6ed
luis <killuari@protonmail.com>
2022-04-10 13:09:28 +0200

    created Android Studio project

---
+++ app/src/main/java/com/example/rem_digits_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rem_digits_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
